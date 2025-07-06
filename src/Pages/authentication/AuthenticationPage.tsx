96ab3d980dbabb998a242d8547b06bfab031dd70
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-07-06 18:04:28 +0530

    adding environment variables

--- src/Pages/authentication/AuthenticationPage.tsx
+++ src/Pages/authentication/AuthenticationPage.tsx
@@ -5,11 +5,11 @@ const AuthenticationPage = () => {
   return (
     <div>
       <MetaPageData
-        title={"Dev Dating | Modern Dating App - Find Love Near You "}
-        description={"This is developer dating platform"}
-        keywords={
+        title="Dev Dating | Modern Dating App - Find Love Near You "
+        description="This is developer dating platform"
+        keywords=
           "dating app, find love, meet singles, online dating, chat, relationships"
-        }
+        
       />
       <Authentication />
     </div>
